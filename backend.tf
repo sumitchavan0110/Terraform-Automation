93a6e479c8d8ee75881967c701692e4113b60c31
sumitchavan0110 <sumitchavan0110@gmail.com>
2023-03-17 17:13:10 +0530

    Changes in backend1

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "mydev-tf-state-bucket-55"
     key = "main"
-    region = "ap-south-1"
+    region = "us-west-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
